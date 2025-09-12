5e39fbcecc73c75a67662b5ca87e33540108d847
kchhq <uim933@naver.com>
2025-09-12 12:32:52 +0900

    OOP 1주차

---
+++ src/ch1/sec08/hello.java
@@ -0,0 +1,8 @@
+package ch1.sec08;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+
+}
---
+++ src/ch2/sec03/FloatDoubleExample.java
@@ -0,0 +1,16 @@
+package ch2.sec03;
+
+public class FloatDoubleExample {
+
+    public static void main(String[] args) {
+        float var1 = 0.123456789f;
+        double var2 = 0.123456789	;
+        System.out.println("var1 : "+var1);
+        System.out.println("var2 : "+var2);
+
+        double var3 = 3e6;
+        float var4 = 3e6f;
+
+    }
+
+}
