a2e492cca81754d11a58c2bfa2faa5dac088f998
kchhq <uim933@naver.com>
2025-09-30 18:39:47 +0900

    OOP 1주차

---
+++ src/Teamwork1/MoneyManagement.java
@@ -0,0 +1,44 @@
+package Teamwork1;
+
+import java.util.Scanner;
+
+public class MoneyManagement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
+        int currentMoney = 0;
+
+        while(run) {
+            System.out.println("------------------------");
+            System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+            System.out.println("-------------------------");
+            System.out.print("선택> ");
+
+            String strNum = scanner.nextLine();
+
+            if(strNum.equals("1")) {
+                System.out.println("예금액>");
+                String depositMoney = scanner.nextLine();
+                currentMoney += Integer.parseInt(depositMoney);
+                System.out.println("잔고> " + currentMoney);
+            }
+            else if(strNum.equals("2")) {
+                System.out.println("출금액>");
+                String withDrawMoney = scanner.nextLine();
+                currentMoney -= Integer.parseInt(withDrawMoney);
+                System.out.println("잔고> " + currentMoney);
+            }
+            else if(strNum.equals("3")) {
+                System.out.println("잔고>" + currentMoney);
+            }
+            else if(strNum.equals("4")) {
+                run = false;
+            }
+            else if(Integer.parseInt(strNum)>=5) {
+                System.out.println("잘못된 숫자를 입력하셨습니다");
+            }
+        }
+        System.out.println("프로그램 종료");
+
+    }
+}
